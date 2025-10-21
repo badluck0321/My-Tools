1045112cf5104dd97d0baf891f71eb123e5c8286
mehdi abousalih <you@example.com>
2025-10-21 12:00:25 +0200

    Initializing Backend using springboot rest api (maven+java)

---
+++ BackEnd-MyTools/src/main/java/com/example/BackEnd_MyTools/BackEndMyToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackEnd_MyTools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndMyToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndMyToolsApplication.class, args);
+	}
+
+}
---
+++ BackEnd-MyTools/src/test/java/com/example/BackEnd_MyTools/BackEndMyToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackEnd_MyTools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndMyToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
