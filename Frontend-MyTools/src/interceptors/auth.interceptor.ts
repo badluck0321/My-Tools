82d382b269aef63fcb1bc5b2bc4bf896a99fe724
mehdi <mehdi@example.com>
2025-12-11 22:25:30 +0000

    feat: Implement Keycloak authentication provider and integrate with app
    - Refactored axios interceptor to use Keycloak for authentication, updating the base URL and adding token handling.
    - Wrapped the main application in a KeycloakProvider to manage authentication state and loading.
    - Created a new KeycloakProvider component to handle Keycloak initialization and provide authentication context.

--- Frontend-MyTools/src/interceptors/auth.interceptor.ts
+++ Frontend-MyTools/src/interceptors/auth.interceptor.ts
@@ -1,25 +1,22 @@
 import axios from "axios";
 import getKeycloakInstance from "../keycloak";
-// import { getKeycloakInstance } from "../keycloak/keycloak"; // <— IMPORTANT
-
 const interceptor = axios.create({
-    baseURL: "http://localhost:8888/api",
+    baseURL: "http://localhost:8888",
     withCredentials: true,
 });
 
 interceptor.interceptors.request.use(async (config) => {
 
     const keycloak = getKeycloakInstance();
-
+    console.log("INTERCEPTOR RAN");
+    console.log("KEYCLOAK TOKEN:", keycloak?.token);
+    console.log("FINAL REQUEST HEADERS BEFORE SEND:", config.headers);
     // Refresh token if needed
     if (keycloak && keycloak.token) {
         // Refresh before expiration
         await keycloak.updateToken(30).catch(() => keycloak.login());
-
         // Attach the token
         config.headers["Authorization"] = "Bearer " + keycloak.token;
-        console.log("Attached Token: " + keycloak.token);
-
     }
 
     return config;
@@ -28,13 +25,3 @@ interceptor.interceptors.request.use(async (config) => {
 export default interceptor;
 
 
-// import axios from "axios";
-
-// // Create axios instance
-// const interceptor = axios.create({
-//     baseURL: "http://localhost:8888/api",
-//     withCredentials: true,
-// });
-
-
-// export default interceptor;
