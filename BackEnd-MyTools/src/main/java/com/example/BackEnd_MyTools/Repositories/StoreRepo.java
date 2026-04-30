2715b6bccbc185f6b2e1022a577692577bb8c1d0
mehdi abousalih <you@example.com>
2026-04-30 12:47:47 +0200

    feat(Store): enhance store creation and retrieval with authentication checks and update store entity structure

--- BackEnd-MyTools/src/main/java/com/example/BackEnd_MyTools/Repositories/StoreRepo.java
+++ BackEnd-MyTools/src/main/java/com/example/BackEnd_MyTools/Repositories/StoreRepo.java
@@ -1,10 +1,15 @@
 package com.example.BackEnd_MyTools.Repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import com.example.BackEnd_MyTools.Entitys.Store;
 
 @Repository
 public interface StoreRepo extends MongoRepository<Store, String> {
+Optional<Store> findByOwnerIdContaining(String ownerId);
+boolean existsByOwnerIdContaining(String ownerId);
+boolean existsByAssociatsIdsContaining(String userId);
 
 }
